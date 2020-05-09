12f803f1438023dbef1d1ca4fb8b71668ee0fb38
Matúš Karahuta <matus123@gmail.com>
2020-05-09 16:02:33 +0100

    Initial commit
    everything apart from the node modules

---
+++ models/product.js
@@ -0,0 +1,25 @@
+const Sequelize = require('Sequelize');
+const sequelize = require('../util/database.js');
+
+const Product = sequelize.define('product', {
+  id:{
+    type:Sequelize.INTEGER,
+    autoIncrement:true,
+    allowNull:false,
+    primaryKey:true
+  },
+  title:Sequelize.STRING,
+  price:{
+    type:Sequelize.DOUBLE,
+    allowNull:false
+  },
+  imageUrl:{
+    type:Sequelize.STRING,
+    allowNull:false
+  },
+  description:{
+    type:Sequelize.STRING,
+    allowNull:false
+  }
+});
+module.exports = Product;
